2e67497e7dc079b9a0fe3bf830e5f339e54d690f
Teresa Vidal Calleja <teresacalleja@eduroam-lee-dock-1-194.ethz.ch>
2016-11-30 16:08:20 +0100

    Added file to learn a pyramid GP to handle multires maps

---
+++ GP_pyramid_example.m
@@ -0,0 +1,108 @@
+clear all; close all;
+
+%% Parameters %%
+
+VISUALIZE = 1;
+NUM_WEEDS = 15;
+PYRAMID = 2;
+DimX = 20;
+DimY = 20;
+
+% Set up GP parameters.
+%covfunc = 'covSEiso';
+covfunc = {'covMaterniso',3};
+likfunc = @likGauss;
+inffunc = @infExact;
+meanfunc = @meanConst;
+% Hyperparameters
+hyp.mean = 0.5;
+hyp.cov = [0.2; 0.3];
+hyp.cov = log(hyp.cov);
+hyp.lik = 1;
+% Number of conjugate gradient steps
+Ncg = -100;
+
+% Generate prediction grid.
+% Prediction it will be at z=1, z=2 and z=3
+[mesh_x,mesh_y,mesh_z] = meshgrid(linspace(1,DimX,100), ...
+    linspace(1,DimY,100),[1:3]);
+Z =  [reshape(mesh_x, numel(mesh_x), 1), reshape(mesh_y, numel(mesh_y), 1), reshape(mesh_z, numel(mesh_z), 1)];
+
+% Create random weed map at a lower resolution.
+grid_map(:,:,1) = create_poisson_map(NUM_WEEDS, DimX, DimY);
+[weed_coordinates_y, weed_coordinates_x] = find(grid_map == 1);
+
+%Create meshgrid with z In training data first layer is at z=1 and second
+%layer is at z=3
+[mesh_x,mesh_y,mesh_z] = meshgrid(linspace(1,DimX,DimX), ...
+    linspace(1,DimY,DimY),[1,3]);
+
+X = [reshape(mesh_x, numel(mesh_x), 1), reshape(mesh_y, numel(mesh_y), 1), reshape(mesh_z, numel(mesh_z), 1)];
+
+
+% Create layers for multiple resolution
+for i=2:PYRAMID
+    tmp_map=impyramid(grid_map(:,:,i-1),'expand');
+    ind = find(tmp_map > 0);
+    tmp_map(ind)=1;
+    grid_map(:,:,i)=tmp_map(1:2:end,1:2:end);
+end
+
+Y = reshape(grid_map,[],1);
+
+
+ 
+%% Inference %%
+
+% Optimise hyperparameters.
+[hyp, ~, ~] = ...
+    minimize(hyp, 'gp', Ncg, inffunc, meanfunc, covfunc, likfunc, X, Y); 
+
+[ymu, ys2 , fmu, fs2] = gp(hyp, inffunc, meanfunc, covfunc, likfunc, ...
+    X, Y, Z);
+ymu = reshape(ymu, 100, 100,3);
+
+
+%% Plotting %%
+if (VISUALIZE)
+   figure
+   subplot(1,2,1)
+   imagesc(grid_map(:,:,1))
+   title('Ground truth weed map')
+   set(gca,'Ydir','Normal');
+    
+   subplot(1,2,2)
+   imagesc(ymu(:,:,1))
+   title('Interpolated weed map')
+   colorbar
+   set(gca,'Ydir','Normal');
+   
+   c = colorbar;
+   caxis([0, 1])
+   set(gcf, 'Position', [-1567, 331, 1247, 507]);
+
+
+   figure
+   subplot(1,2,1)
+   imagesc(grid_map(:,:,2))
+   title('Ground truth weed map')
+   set(gca,'Ydir','Normal');
+    
+   subplot(1,2,2)
+   imagesc(ymu(:,:,3))
+   title('Interpolated weed map')
+   colorbar
+   set(gca,'Ydir','Normal');
+   
+   c = colorbar;
+   caxis([0, 1])
+   set(gcf, 'Position', [-1567, 331, 1247, 507]);
+   
+   figure
+   imagesc(ymu(:,:,2))
+   title('Interpolated weed map at not observed z')
+   colorbar
+   set(gca,'Ydir','Normal');
+   
+
+end
